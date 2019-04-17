4c76167810a80c58426a27fc90cfee0a9bd1683c
abitravers1989 <atravers4007@gmail.com>
2019-04-17 14:49:14 +0100

    add inital basic tested functionality

--- src/imagePesister.js
+++ src/imagePesister.js
@@ -1,5 +1,10 @@
 module.exports = ({uuidv4}) => ({
   saveImage: (image) => {
+    if (!image) {
+      throw new Error('No image is provided');
+    }
+    // if image ==typeOf Images throw error
+    console.log('----->', image)
     return uuidv4();
   },
   retrieveImage: (id) => {},
--- src/imagePesister.test.js
+++ src/imagePesister.test.js
@@ -1,6 +1,8 @@
 const chai = require('chai');
 const uuidv4 = require('uuid/v4');
+
 const expect = chai.expect
+chai.use(require('chai-uuid'));
 
 //Factory pattern
 const imagePesisterFactory = require('./imagePesister');
@@ -11,12 +13,23 @@ const imagePesister = imagePesisterFactory(dependencies);
 
 describe('Image Pesister', () => {
   describe('saveImage', () => {
+    it('throws an error if no image is provided', () => {
+      expect(() => imagePesister.saveImage()).to.throw('No image is provided');
+    });
+
+    it('throws an error if image input is not an image', () => {
+      const image = 'image';
+      expect(() => imagePesister.saveImage()).to.throw('Not type of image');
+    });
+
     it('saves the image successfully', () => {
 
     });
+
     it('returns a unique ID', () => {
       // expect it returns a typeOf uuid 
-      expect(imagePesister.saveImage()).to.equal()
+      const image = 'image';
+      expect(imagePesister.saveImage(image)).to.be.a.uuid('v4');
 
       // does not return the same ID twice
     });
