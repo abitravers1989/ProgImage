59894c19ebfa71e125e302788b79cdafecbe57d4
abitravers1989 <atravers4007@gmail.com>
2019-04-17 16:57:11 +0100

    add integration test skeleton

---
+++ test/integration/imagePesisterLocalFileSystem.text.js
@@ -0,0 +1,7 @@
+const { uuidv4 } = require('../../src/container');
+
+describe('image pesister', () => {
+  it('reads an image and saves it to the local file system', () => {});
+  it('returns a unique identifier', () => {});
+  it('saves the image at a path the same as the unique identifier', () => {});
+})
