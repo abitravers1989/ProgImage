e873c3c0d56792503486b1b1e5532324952ce6c7
abitravers1989 <atravers4007@gmail.com>
2019-04-30 21:13:11 +0100

    WIP image transformation

--- src/repositories/fileSystemImagePersister.js
+++ src/repositories/fileSystemImagePersister.js
@@ -3,20 +3,11 @@ module.exports = ({ fileSystem, uniqueIDGenerator, envVariables }) => ({
     // add some image validation and refactor this out
     // this is an uncaught error at the min .. need to catch this error in route and return it
     if (!imageData) throw new Error('No image is provided');
-    // benefits of making this a promise?
     const imageID = uniqueIDGenerator();
-    // try {
-    // this error should instead be handled in ROUTE so can return error?
     fileSystem.writeFileSync(
       `${envVariables.IMAGESTOREPATH}/${imageID}`,
       imageData,
     );
-    // } catch (error) {
-    //   console.error('Error', error);
-    // }
     return imageID;
   },
 });
-
-// how to get the file extension:
-// .split('.').pop().toLowerCase()
--- src/rest/routes/images.js
+++ src/rest/routes/images.js
@@ -1,29 +1,28 @@
-module.exports = ({ logger, fileSystemImageRetriever, sharp }) => ({
+module.exports = ({
+  logger,
+  fileSystemImageRetriever,
+  sharp,
+  fileSystem,
+  envVariables,
+}) => ({
   getImage: (req, res) => {
     const requestedImage = req.query.imageID;
-   
     const desiredImageExtension = requestedImage.split('.')[1];
-    console.log('----->desiredImageExtension', desiredImageExtension)
-    
-      const imageId = requestedImage.split('.')[0];
-    
-    let returnImage;
-    let transformedImage
-    try {  
-      console.log('-----?????>', imageId)
-      returnImage = fileSystemImageRetriever.getImage(imageId);
-      console.log('----->desiredImageExtension', desiredImageExtension)
+    const imageId = requestedImage.split('.')[0];
+
+    // let returnImage;
+    let transformedImage;
+    try {
+      const returnImage = fileSystemImageRetriever.getImage(imageId);
       if (!desiredImageExtension) {
-        return res.status(200).json(returnImage)
+        return res.status(200).json(returnImage);
       }
       transformedImage = sharp(returnImage)
-      .toFormat(desiredImageExtension)
-      .toBuffer((error, data, info) => {
-        if(error) logger.error(error)
-        console.log('-----data>', data)
-        return data //.options.input.buffer.data
-      })
-      console.log('------>jkjkjkjkj', transformedImage.options.input)
+        .toFormat(desiredImageExtension)
+        .toBuffer((error, data) => {
+          if (error) logger.error(error);
+          return data; // .options.input.buffer.data
+        });
     } catch (error) {
       logger.error(error.message);
       if (error.message.includes('ENOENT')) {
@@ -36,6 +35,10 @@ module.exports = ({ logger, fileSystemImageRetriever, sharp }) => ({
       return res.status(404).send(error.message);
       // return next(error);
     }
+    fileSystem.writeFileSync(
+      `${envVariables.IMAGESTOREPATH}/${imageId}.${desiredImageExtension}`,
+      JSON.stringify(transformedImage.options.input.buffer),
+    );
     // logger.info('Successfully returned image from ID:', imageID)
     return res.status(200).json(transformedImage.options.input.buffer);
   },
