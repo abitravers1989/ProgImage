9cc4c90a6943930bdd17361b2519106c60dfb1cd
abitravers1989 <atravers4007@gmail.com>
2019-05-01 12:55:52 +0100

    remove todo

--- src/rest/routes/images.js
+++ src/rest/routes/images.js
@@ -15,8 +15,6 @@ module.exports = ({ logger, fileSystemImageRetriever, imageTransformer }) => ({
       res.status(200).json(requestedImage);
     } catch (error) {
       logger.error(error.message);
-      // TODO pass this error to next(error) and handle it in index;
-      // chang this so it calculates the status 
       res.status(400).send(error.message);
     }
   },
--- src/rest/routes/images.test.js
+++ src/rest/routes/images.test.js
@@ -12,11 +12,14 @@ describe('routes/images', () => {
     fileSystemImageRetriever: {
       getImage: sandbox.stub(),
     },
+    imageTransformer: {
+      convertImage: sandbox.stub(),
+    },
     // fileSystemImagePersister: {},
   };
 
   const imagesRoute = imagesRoutesFactory(dependencies);
-  const { fileSystemImageRetriever, logger } = dependencies;
+  const { fileSystemImageRetriever, logger, imageTransformer } = dependencies;
 
   const res = {
     status: sandbox.stub(),
@@ -32,112 +35,178 @@ describe('routes/images', () => {
     res.status.returnsThis();
   });
 
-  describe('when the given imageID is matched to a valid stored image', () => {
-    // eg http://localhost:3000/getImage?imageID=dd301786-9b3a-4972-8be6-e77f2763eaf2
-    it('returns a 200 and the valid image', () => {
-      const imageID = uniqueIDGenerator();
-      const req = {
-        query: {
-          imageID,
-        },
-      };
-      const expectedImage = '�.j�a/���K������-�KO>�W��&���aa�';
+  describe('getImage', () => {
+    describe('when no desired image extension is given', () => {
+      describe('when the given imageID is matched to a valid stored image', () => {
+        // eg http://localhost:3000/getImage?imageID=dd301786-9b3a-4972-8be6-e77f2763eaf2
+        it('returns a 200 and the valid image', () => {
+          const imageID = uniqueIDGenerator();
+          const req = {
+            query: {
+              imageID,
+            },
+          };
+          const expectedImage = '�.j�a/���K������-�KO>�W��&���aa�';
+          fileSystemImageRetriever.getImage.returns(expectedImage);
 
-      fileSystemImageRetriever.getImage.returns(expectedImage);
+          imagesRoute.getImage(req, res);
 
-      imagesRoute.getImage(req, res);
+          expect(
+            fileSystemImageRetriever.getImage,
+          ).to.have.been.calledWithExactly(imageID);
+          expect(res.status).to.have.been.calledWithExactly(200);
+          expect(res.json).to.have.been.calledWithExactly(expectedImage);
+        });
+      });
 
-      expect(fileSystemImageRetriever.getImage).to.have.been.calledWithExactly(
-        imageID,
-      );
-      expect(res.status).to.have.been.calledWithExactly(200);
-      expect(res.json).to.have.been.calledWithExactly(expectedImage);
-    });
-  });
+      describe('when the call to the file system fails', () => {
+        describe('when no imageID is provided', () => {
+          // e.g, http://localhost:3000/getImage?imageID=
+          it('returns a 400 with an error message', () => {
+            const req = {
+              query: {
+                imageID: '',
+              },
+            };
 
-  describe('when the call to the file system fails', () => {
-    describe('when no imageID is provided', () => {
-      // e.g, http://localhost:3000/getImage?imageID=
-      it('returns a 400 with an error message', () => {
-        const req = {
-          query: {
-            imageID: '',
-          },
-        };
+            const error = new Error('No image ID has been provided.');
+            fileSystemImageRetriever.getImage.throws(error);
 
-        const error = new Error('No image ID has been provided.');
+            imagesRoute.getImage(req, res);
 
-        fileSystemImageRetriever.getImage.throws(error);
-        imagesRoute.getImage(req, res);
+            expect(
+              fileSystemImageRetriever.getImage,
+            ).to.have.been.calledWithExactly(req.query.imageID);
+            expect(logger.error).to.have.been.calledWithExactly(
+              'No image ID has been provided.',
+            );
+            expect(res.status).to.have.been.calledWithExactly(400);
+            expect(res.send).to.have.been.calledWithExactly(
+              'No image ID has been provided.',
+            );
+          });
+        });
 
-        expect(
-          fileSystemImageRetriever.getImage,
-        ).to.have.been.calledWithExactly(req.query.imageID);
-        expect(logger.error).to.have.been.calledWithExactly(
-          'No image ID has been provided.',
-        );
-        expect(res.status).to.have.been.calledWithExactly(400);
-        expect(res.send).to.have.been.calledWithExactly(
-          'No image ID has been provided.',
-        );
+        describe('when the given imageID does not match a stored image', () => {
+          // eg. http://localhost:3000/getImage?imageID=dd301786-9b3a-4972-89e6-e77f2763eaf2
+          it('returns a 400 and an error message', () => {
+            const imageID = uniqueIDGenerator();
+            const req = {
+              query: {
+                imageID,
+              },
+            };
+
+            const error = new Error(
+              'There is no image at the provided ID, please ensure it is correct.',
+            );
+            fileSystemImageRetriever.getImage.throws(error);
+
+            imagesRoute.getImage(req, res);
+
+            expect(
+              fileSystemImageRetriever.getImage,
+            ).to.have.been.calledWithExactly(req.query.imageID);
+            expect(logger.error).to.have.been.calledWithExactly(
+              'There is no image at the provided ID, please ensure it is correct.',
+            );
+            expect(res.status).to.have.been.calledWithExactly(400);
+            expect(res.send).to.have.been.calledWithExactly(
+              'There is no image at the provided ID, please ensure it is correct.',
+            );
+          });
+        });
+
+        describe('when the given imageID is not a uuid', () => {
+          // eg. http://localhost:3000/getImage?imageID=randomstring
+          it('returns a 400 and an error message', () => {
+            const imageID = 'randomString';
+            const req = {
+              query: {
+                imageID,
+              },
+            };
+
+            const error = new Error('A valid image ID must be provided.');
+            fileSystemImageRetriever.getImage.throws(error);
+
+            imagesRoute.getImage(req, res);
+
+            expect(
+              fileSystemImageRetriever.getImage,
+            ).to.have.been.calledWithExactly(req.query.imageID);
+            expect(logger.error).to.have.been.calledWithExactly(
+              'A valid image ID must be provided.',
+            );
+            expect(res.status).to.have.been.calledWithExactly(400);
+            expect(res.send).to.have.been.calledWithExactly(
+              'A valid image ID must be provided.',
+            );
+          });
+        });
       });
     });
 
-    describe('when the given imageID does not match a stored image', () => {
-      // eg. http://localhost:3000/getImage?imageID=dd301786-9b3a-4972-89e6-e77f2763eaf2
-      it('returns a 400 and an error message', () => {
-        const imageID = uniqueIDGenerator();
-        const req = {
-          query: {
-            imageID,
-          },
-        };
+    describe('when a desired image type is given', () => {
+      // test for when an invalid image is provided is covered above, same thing will happen with or without image conversion type
+      describe('when a valid image is provided', () => {
+        describe('when a valid image conversion type is given', () => {
+          it('returns a 200 and the valid converted image', async () => {
+            const imageID = uniqueIDGenerator();
+            const desiredConversion = 'jpeg';
+            const req = {
+              query: {
+                imageID: `${imageID}.${desiredConversion}`,
+              },
+            };
 
-        const error = new Error(
-          'There is no image at the provided ID, please ensure it is correct.',
-        );
+            const nonConvertedImage = '�.j�a/���K������-�KO>�W��&���aa�';
+            fileSystemImageRetriever.getImage.returns(nonConvertedImage);
+            const convertedImage = '[37, 11, 45, 2]';
+            imageTransformer.convertImage.returns(convertedImage);
 
-        fileSystemImageRetriever.getImage.throws(error);
-        imagesRoute.getImage(req, res);
+            await imagesRoute.getImage(req, res);
 
-        expect(
-          fileSystemImageRetriever.getImage,
-        ).to.have.been.calledWithExactly(req.query.imageID);
-        expect(logger.error).to.have.been.calledWithExactly(
-          'There is no image at the provided ID, please ensure it is correct.',
-        );
-        expect(res.status).to.have.been.calledWithExactly(400);
-        expect(res.send).to.have.been.calledWithExactly(
-          'There is no image at the provided ID, please ensure it is correct.',
-        );
-      });
-    });
+            expect(
+              imageTransformer.convertImage,
+            ).to.have.been.calledWithExactly(
+              nonConvertedImage,
+              desiredConversion,
+            );
+            expect(res.status).to.have.been.calledWithExactly(200);
+            expect(res.json).to.have.been.calledWithExactly(convertedImage);
+          });
+        });
 
-    describe('when the given imageID is not a uuid', () => {
-      // eg. http://localhost:3000/getImage?imageID=randomstring
-      it('returns a 400 and an error message', () => {
-        const imageID = 'randomString';
-        const req = {
-          query: {
-            imageID,
-          },
-        };
+        describe('when a invalid image conversion type is given', () => {
+          it('returns a 400 and error message', async () => {
+            const imageID = uniqueIDGenerator();
+            const desiredConversion = 'svg';
+            const req = {
+              query: {
+                imageID: `${imageID}.${desiredConversion}`,
+              },
+            };
 
-        const error = new Error('A valid image ID must be provided.');
-        fileSystemImageRetriever.getImage.throws(error);
-        imagesRoute.getImage(req, res);
+            const nonConvertedImage = '�.j�a/���K������-�KO>�W��&���aa�';
+            fileSystemImageRetriever.getImage.returns(nonConvertedImage);
+            const error = new Error('Invalid image extension provided.');
+            imageTransformer.convertImage.throws(error);
 
-        expect(
-          fileSystemImageRetriever.getImage,
-        ).to.have.been.calledWithExactly(req.query.imageID);
-        // returns There is no image at the provided ID, please ensure it is correct
-        expect(logger.error).to.have.been.calledWithExactly(
-          'A valid image ID must be provided.',
-        );
-        expect(res.status).to.have.been.calledWithExactly(400);
-        expect(res.send).to.have.been.calledWithExactly(
-          'A valid image ID must be provided.',
-        );
+            await imagesRoute.getImage(req, res);
+
+            expect(
+              imageTransformer.convertImage,
+            ).to.have.been.calledWithExactly(
+              nonConvertedImage,
+              desiredConversion,
+            );
+            expect(res.status).to.have.been.calledWithExactly(400);
+            expect(res.send).to.have.been.calledWithExactly(
+              'Invalid image extension provided.',
+            );
+          });
+        });
       });
     });
   });
